1c352a6930063b16e44b6927adebb2ab8dceff7e
Nicholas Corder <niccorder25@gmail.com>
2017-01-17 03:04:54 -0800

    Utilities

--- docs/README.md
+++
@@ -1,3 +0,0 @@
-# Github Pages presenatiton files.
-
-Uses the awesome (Reveal.js)[https://github.com/hakimel/reveal.js] library to display some informative slides about this project, and the reasons for each decision.
---
+++ utils/fill-items.py
@@ -0,0 +1,26 @@
+import requests
+import random
+
+baseUrl = "http://api.niccorder.me/item"
+itemNames = [
+        ["Apple", 3.50, "A delicious fruit."],
+        ["Cheese", 2.0, "Makes the world go round."],
+        ["Liquor", 12.0, "I'd rather have a bottle infront of me..."],
+        ["Pancakes", 1000, "The food of kings."],
+        ["Soylent", 2.50, "A supplement bevarage."],
+        ["Beets", .40, "You drop these."],
+        ["Bears", 1020.03, "A big russian dog."],
+]
+
+def addItemsRandomly():
+    for i in range(0, 50):
+        randomItem = itemNames[random.randint(0, 6)]
+        requests.post(baseUrl, data={ 
+            'id':i, 
+            'name':randomItem[0],
+            'price':randomItem[1],
+            'description':randomItem[2]
+        })
+
+
+addItemsRandomly()
